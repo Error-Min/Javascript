ca2aa4ca62bb8fcc37ec1dd25c56a5cda56f3ac7
Error-Min <smgm030@gmail.com>
2020-08-25 15:59:27 +0900

    2020-08-25 String tpye

---
+++ 6. JavaScript String type/index.js
@@ -0,0 +1,20 @@
+//문자열
+console.log("안녕");
+
+console.log("I'm Tron man");
+console.log("He said \"I'm Iron man\"");
+console.log('He said "I\'m Iron man"');
+console.log("He said \"I\'m Iron man\"");
+console.log('He said "\I\'m Iron man\"');
+
+console.log(`"He said "I'm Iron man"`);
+
+console.log("Hello" + "World");
+
+console.log("3" + "5");
+
+
+console.log(`한국 영화 역사상 아카데미상을 받은 것은 '기생충'이 처음이다.
+아리스토텔레스는 "인간은 사회적 동물이다."라고 말했다.`);
+
+console.log(`영화 '베테랑'에서 "어이가 없네~"라는 대사가 유명했다.`);
